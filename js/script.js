65eec3e13b0b46b527be62d5bfc88bca11680363
Mahmoud El Kotoury <melkotoury@gmail.com>
2016-05-03 01:55:46 +0200

    footer finished

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
 /**
  * Created by shatta on 5/2/2016.
  */
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("loginModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
